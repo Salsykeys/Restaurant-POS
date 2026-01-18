2d294ce028a4d78f5e8d04564094d92e15412326
Salsyekys <salllwaduh@gmail.com>
2026-01-18 17:49:31 +0700

    first commit

---
+++ pos-frontend/src/pages/Tables.jsx
@@ -0,0 +1,12 @@
+import BottomNav from "../components/shared/BottomNav"
+
+const Tables = () => {
+  return (
+    <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden">
+      Tables
+      <BottomNav/>
+    </section>
+  )
+}
+
+export default Tables
